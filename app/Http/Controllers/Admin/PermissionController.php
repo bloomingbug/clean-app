f7677b3bc705245e3559f47aacc7621fd88b2997
Tarmuji <tarmuji1514@gmail.com>
2024-08-28 22:58:56 +0700

    FEAT: Role and Permission

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Yajra\DataTables\Facades\DataTables;
+use Yajra\DataTables\Html\Button;
 
 class PermissionController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('permission:permission.index')->only('index');
+    }
+
+    public function index(Request $request)
+    {
+        if ($request->wantsJson()) {
+            $permissions = Permission::orderBy('name', 'asc')->get();
+            return DataTables::of($permissions)
+                ->addIndexColumn()
+                ->make(true);
+        } else {
+            return view('pages.admin.permission.index');
+        }
+    }
 }
---
+++ resources/views/pages/admin/permission/index.blade.php
@@ -0,0 +1,21 @@
+<x-layouts.admin-layout title="Permission" :vite="['resources/js/pages/admin/permission/index.js']">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="datatable" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.admin-layout>
